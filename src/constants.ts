c775c870e5806a4c90f578f5a4493943395b9939
Konstantin Kostin <kkostin@funnelfox.com>
2026-01-06 21:58:57 +0200

    change version to 0.5.0-beta.3

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@
 import type { CheckoutStyle } from '@primer-io/checkout-web';
 import { PaymentMethod } from './enums';
 
-export const SDK_VERSION = '0.5.0';
+export const SDK_VERSION = '0.5.0-beta.3';
 
 export const DEFAULTS = {
   BASE_URL: 'https://billing.funnelfox.com',
